536202c49e7f52b5986dc3b993cf5a07ce8ac26d
Prashiddha Dhoj Thapa <thapa_prash@yahoo.com>
2023-04-17 20:14:44 +0545

    Minor Improvements and Added Documentation
    - Added source documentation.
    - Improved variable names.
    - Added type hinting.
    - Added genres to movie page.
    - Added README.md for details on the project.

--- README.md
+++ README.md
@@ -1,15 +1,45 @@
-# Neural-Search-with-Qdrant
+# Movie NeuralBase
 
 ## Introduction
-
->TODO
+Movie NeuralBase is an AI-based web-app that can be used to search and find movie recommendations. The 
 
 
 ## Software requirements
 
 - pip@23.1
-- python@3.8.5
+- python@3.11.0
 
 ## Setup and Deployment
 
->TODO
+### Installing neural_base package
+
+Clone the Neural-Search-with-Qdrant repository and create a virtual environment using `venv` module:
+​
+```
+bash
+foo@bar Neural-Search-with-Qdrant:~$ python3.11 -m venv .venv/
+foo@bar Neural-Search-with-Qdrant:~$ source .venv/bin/activate
+```
+
+
+>**NOTE:**
+>The virtual environment can be deactivated using the `deactivate` command.
+```
+console
+(.venv) foo@bar scnx-py-minstrel:~$ deactivate
+```
+
+
+Install the neural_search package:
+```
+console
+(.venv) foo@bar Neural-Search-with-Qdrant:~$ pip install -e .
+```
+
+Install the neural_search package:
+```
+console
+(.venv) foo@bar Neural-Search-with-Qdrant:~$ pip install -e .
+```
+
+##
--- neural_search/upload.py
+++ neural_search/upload.py
@@ -2,7 +2,20 @@ from qdrant_client.models import VectorParams
 from qdrant_client import QdrantClient
 from neural_search.config import host, api_key
 
-def upload_data(qdrant_client, collection_name, vectors, payload=None, ids=None):
+from typing import List, Optional
+
+
+def upload_data(
+    qdrant_client: QdrantClient,
+    collection_name: str,
+    vectors: List,
+    payload: Optional[List] = None,
+    ids: Optional[List] = None,
+) -> None:
+    """
+    Uploads Points to a collect on the provided Qdrant cluster.
+
+    """
     size = vectors.shape[1]
     re_init_collection(qdrant_client, collection_name, size)
 
@@ -11,21 +24,32 @@ def upload_data(qdrant_client, collection_name, vectors, payload=None, ids=None)
         vectors=vectors,
         payload=payload,
         ids=ids,
-        batch_size=256,  # How many vectors will be uploaded in a single request?
+        batch_size=256,
     )
 
 
-def re_init_collection(qdrant_client, collection_name, size):
+def re_init_collection(qdrant_client: QdrantClient, collection_name: str, size) -> None:
+    """
+    Re-initialises the collection, ensuring it doesn't consist of old data. As the free-tier Qdrant
+    cluster only offers 1GB of memory, the on_disk_payload has been set to True which stores the
+    payload to disk instead of memory.
+
+    """
     qdrant_client.recreate_collection(
         collection_name=collection_name,
         vectors_config=VectorParams(size=size, distance="Cosine"),
-        on_disk_payload=True
+        on_disk_payload=True,
     )
 
-def establish_conn():
+
+def establish_conn() -> QdrantClient:
+    """
+    Connects to the Qdrant cluster.
+
+    """
     if api_key:
         qdrant_client = QdrantClient(url=host, api_key=api_key)
     else:
         qdrant_client = QdrantClient(host=host)
-    
+
     return qdrant_client
